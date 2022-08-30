c0128e2013c79d75b5562bb31f6e66ea6166e5d4
Oleksandr Galchyn <meison543@gmail.com>
2022-08-30 17:36:27 +0200

    color and search function fixes

--- apps/admin/lib/src/features/product_editing/presentation/widgets/product_information_view/country_dropdown.dart
+++ apps/admin/lib/src/features/product_editing/presentation/widgets/product_information_view/country_dropdown.dart
@@ -1,5 +1,6 @@
 import 'package:bottleshop_admin/src/core/presentation/providers/providers.dart';
 import 'package:bottleshop_admin/src/features/product_editing/presentation/providers/providers.dart';
+import 'package:diacritic/diacritic.dart';
 import 'package:flutter/material.dart';
 import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -12,71 +13,75 @@ class CountryDropdown extends HookWidget {
     Key? key,
   }) : super(key: key);
 
-  bool searchFunction(String item, String filter) {
-    item = item.toLowerCase();
-    filter = filter.toLowerCase();
-    item = item.replaceAll('ä', 'a');
-    item = item.replaceAll('á', 'a');
-    item = item.replaceAll('č', 'c');
-    item = item.replaceAll('ď', 'd');
-    item = item.replaceAll('é', 'e');
-    item = item.replaceAll('í', 'i');
-    item = item.replaceAll('ĺ', 'l');
-    item = item.replaceAll('ľ', 'l');
-    item = item.replaceAll('ň', 'n');
-    item = item.replaceAll('ó', 'o');
-    item = item.replaceAll('ô', 'o');
-    item = item.replaceAll('ŕ', 'r');
-    item = item.replaceAll('š', 's');
-    item = item.replaceAll('ť', 't');
-    item = item.replaceAll('ú', 'u');
-    item = item.replaceAll('ý', 'y');
-    item = item.replaceAll('ž', 'z');
-    return item.contains(filter);
-  }
-
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.only(top: 16, bottom: 16),
-      child: DropdownSearch<String>(
-        filterFn: searchFunction,
-        popupProps:
-            PopupProps.menu(showSelectedItems: true, showSearchBox: true),
-        items: useProvider(constantAppDataProvider)
-            .state
-            .countries
-            .map((e) => e.localizedName.local)
-            .toList(),
-        validator: (val) {
-          if (val == null) {
-            return 'Musí byť vybraná krajina';
-          }
-          return null;
-        },
-        selectedItem: useProvider(
-          editedProductProvider.select((value) => value.state.country?.localizedName.local),
+      child: Theme(
+        data: ThemeData(
+          textTheme: TextTheme(subtitle1: TextStyle(color: Colors.black, fontSize: 12)),
         ),
-        dropdownDecoratorProps: DropDownDecoratorProps(
-          textAlignVertical: TextAlignVertical.center,
-          dropdownSearchDecoration: InputDecoration(
-            hintText: '* Krajina',
-            filled: true,
-            fillColor: AppTheme.lightGreySolid,
-          )
-        ),
-        onChanged: (value) {
-          context.read(editedProductProvider).state = context
-              .read(editedProductProvider)
+        child: DropdownSearch<String>(
+          filterFn: ((item, filter) {
+            item = item.toLowerCase();
+            filter = filter.toLowerCase();
+            item = removeDiacritics(item);
+            filter = removeDiacritics(filter);
+            return item.contains(filter);
+          }),
+          popupProps: PopupProps.menu(
+              showSelectedItems: true,
+              showSearchBox: true,
+              itemBuilder: (
+                context,
+                item,
+                isSelected,
+              ) {
+                return Container(
+                  margin: EdgeInsets.symmetric(horizontal: 3),
+                  child: ListTile(
+                    selected: false,
+                    title: Text(item),
+                    textColor: Colors.black,
+                  ),
+                );
+              }),
+          items: useProvider(constantAppDataProvider)
               .state
-              .copyWith(
-                  country: context
-                      .read(constantAppDataProvider)
-                      .state
-                      .countries
-                      .firstWhere(
-                          (element) => element.localizedName.local == value));
-        },
+              .countries
+              .map((e) => e.localizedName.local)
+              .toList(),
+          validator: (val) {
+            if (val == null) {
+              return 'Musí byť vybraná krajina';
+            }
+            return null;
+          },
+          selectedItem: useProvider(
+            editedProductProvider
+                .select((value) => value.state.country?.localizedName.local),
+          ),
+          dropdownDecoratorProps: DropDownDecoratorProps(
+              textAlignVertical: TextAlignVertical.center,
+              dropdownSearchDecoration: InputDecoration(
+                hintText: '* Krajina',
+                filled: true,
+                fillColor: AppTheme.lightGreySolid,
+              )
+            ),
+          onChanged: (value) {
+            context.read(editedProductProvider).state = context
+                .read(editedProductProvider)
+                .state
+                .copyWith(
+                    country: context
+                        .read(constantAppDataProvider)
+                        .state
+                        .countries
+                        .firstWhere(
+                            (element) => element.localizedName.local == value));
+          },
+        ),
       ),
     );
   }
