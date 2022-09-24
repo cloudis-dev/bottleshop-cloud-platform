6683866288e065c38fcfd39b12f39487ff9d6af2
Kristián Balaj <kiko.balaj@gmail.com>
2022-09-24 15:38:10 +0200

    feat: setup firebase correctly

--- apps/admin/lib/main.dart
+++ apps/admin/lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
-import 'dart:io';
-import 'dart:isolate';
+
+import 'package:bottleshop_admin/firebase_options.dart';
 import 'package:bottleshop_admin/src/my_app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
@@ -12,14 +12,14 @@ void main() async {
   await runZonedGuarded<Future<void>>(() async {
     WidgetsFlutterBinding.ensureInitialized();
     await Firebase.initializeApp(
-      options: FirebaseOptions(apiKey: "AIzaSyATDXyya2cT8D2V1ZFDE83ewlpUFwRZC0U", appId: "1:525277285012:web:79a4cedc090bcaac0ec281", messagingSenderId: "525277285012", projectId: "bottleshop-3-veze-dev-54908", storageBucket: "bottleshop-3-veze-dev-54908.appspot.com")
-       );
+        options: DefaultFirebaseOptions.currentPlatform);
 
     FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
 
     runApp(ProviderScope(child: MyApp()));
   },
- 
-      (error, stack) => kIsWeb ? print(error) :
-          FirebaseCrashlytics.instance.recordError(error, stack, fatal: true));
+      (error, stack) => kIsWeb
+          ? print(error)
+          : FirebaseCrashlytics.instance
+              .recordError(error, stack, fatal: true));
 }
