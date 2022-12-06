4a61a8a9a2ab19438a40005cb994afba1f64c9f5
Kristián Balaj <kiko.balaj@gmail.com>
2022-12-06 15:56:36 +0100

    feat: bump deps

--- apps/bottleshop-api/src/functions/post-feedback.ts
+++ apps/bottleshop-api/src/functions/post-feedback.ts
@@ -1,13 +1,11 @@
-
 import * as functions from 'firebase-functions';
 
 import { tier1Region } from '../constants/other';
 
 export const postFeedback = functions
   .region(tier1Region)
-  .runWith({ memory: '512MB' })
+  .runWith({ allowInvalidAppCheckToken: true })
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   .https.onCall(async (_, context) => {
     functions.logger.error('hello from post feedback');
-    return {};
   });
